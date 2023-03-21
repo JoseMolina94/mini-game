d0cc6feb63a4c741bcee4354dcee7ba257444544
dev06 <dev06@codecraftdev.com>
2023-03-21 15:06:28 -0500

    Ya tiene obstaculos basicos

--- src/entities/ObstacleBall/index.js
+++ src/entities/ObstacleBall/index.js
@@ -5,18 +5,24 @@ export const ObstacleBall = ({
   direction = [],
   scale= 1
 }) => {
-  const velocity = 50
+  const maxVelocity = 100
+  
+  const setVelocity = () => {
+    return Math.floor(Math.random() * (maxVelocity - 10) + 10)
+  }
   
   const update = ({entity, deltaTime}) => {
-    if (direction.length > 0) {
-      for (const dir of direction) {
+    if (entity.direction.length > 0) {
+      const velocity = setVelocity()
+      
+      for (const dir of entity.direction) {
         // console.log ('FF', dir[1], entity)
         if (dir.includes('-')) {
           const axi = dir[1]
           entity.position[axi] -= velocity * deltaTime
           entity.rotation[axi] -= velocity * deltaTime
           
-          if (entity.position[axi] <= -95) {
+          if (entity.position[axi] <= -35) {
             entity.position.x = position.x
             entity.position.y = position.y
             entity.position.z = position.z
@@ -26,7 +32,7 @@ export const ObstacleBall = ({
           entity.position[dir] += velocity * deltaTime
           entity.rotation[dir] += velocity * deltaTime
   
-          if (entity.position[dir] >= 95) {
+          if (entity.position[dir] >= 35) {
             entity.position.x = position.x
             entity.position.y = position.y
             entity.position.z = position.z
@@ -49,11 +55,12 @@ export const ObstacleBall = ({
   }
   
   return {
-    velocity,
+    maxVelocity,
     update,
     init,
     asset: 'assets/obsball.glb',
     key: 'obs-ball',
-    name: 'ball'
+    name: 'ball',
+    direction
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,9 @@ import {DeathDuel} from "../systems/DeathDuel";
 import {ObstacleBall} from "../entities/ObstacleBall";
 
 export default function Home() {
+  const BALL_SCALE = 0.5
+  const BALL_Y_POS = 5
+  const BALL_POS_INIT = 35
   
   useEffect(() => {
     const player = Cookies.get(`${process.env.NEXT_PUBLIC_CURRENT_PLAYER_COOKIE_NAME}`) || ''
@@ -31,10 +34,10 @@ export default function Home() {
         Ground({asset: 'assets/g7.glb', position: {x: 25, y: 0, z: 25}, scale: 5}),
         Ground({asset: 'assets/g8.glb', position: {x: 25, y: 0, z: -25}, scale: 5}),
         Ground({asset: 'assets/g9.glb', position: {x: -25, y: 0, z: 25}, scale: 5}),
-        ObstacleBall({direction: ['-x'], position: {x: 95, y: 7, z: 0}, scale: 0.75}),
-        ObstacleBall({direction: ['z'], position: {x: 0, y: 7, z: -95}, scale: 0.75}),
-        ObstacleBall({direction: ['x'], position: {x: -95, y: 7, z: 0}, scale: 0.75}),
-        ObstacleBall({direction: ['-z'], position: {x: 0, y: 7, z: 95}, scale: 0.75})
+        ObstacleBall({direction: ['-x'], position: {x: BALL_POS_INIT, y: BALL_Y_POS, z: 0}, scale: BALL_SCALE}),
+        ObstacleBall({direction: ['z'], position: {x: 0, y: BALL_Y_POS, z: -BALL_POS_INIT}, scale: BALL_SCALE}),
+        ObstacleBall({direction: ['x'], position: {x: -BALL_POS_INIT, y: BALL_Y_POS, z: 0}, scale: BALL_SCALE}),
+        ObstacleBall({direction: ['-z'], position: {x: 0, y: BALL_Y_POS, z: BALL_POS_INIT}, scale: BALL_SCALE})
       ]}
       systems={[
         Controls(),
